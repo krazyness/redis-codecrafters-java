59d8835bc9ea386a9451990b27af8edcd8a82899
krazyness <brodyxdai@gmail.com>
2025-07-26 18:14:52 -0400

    Refactor server socket handling to support multiple client connections

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -9,14 +9,32 @@ public class Main {
     System.out.println("Logs from your program will appear here!");
 
     ServerSocket serverSocket = null;
-    Socket clientSocket = null;
     int port = 6379;
     try {
       serverSocket = new ServerSocket(port);
       serverSocket.setReuseAddress(true);
-      // Wait for connection from client.
-      clientSocket = serverSocket.accept();
 
+      while (true) {
+        Socket clientSocket = serverSocket.accept();
+        Thread clientThread = new Thread(() -> clientHandler(clientSocket));
+        clientThread.start();
+      }
+
+    } catch (IOException e) {
+      System.out.println("IOException: " + e.getMessage());
+    } finally {
+      try {
+        if (serverSocket != null) {
+          serverSocket.close();
+        }
+      } catch (IOException e) {
+        System.out.println("IOException: " + e.getMessage());
+      }
+    }
+  }
+
+  private static void clientHandler(Socket clientSocket) {
+    try {
       OutputStream outputStream = clientSocket.getOutputStream();
 
       while (true) {
@@ -25,14 +43,11 @@ public class Main {
         String input = new String(buffer);
         outputStream.write("+PONG\r\n".getBytes());
       }
-
     } catch (IOException e) {
       System.out.println("IOException: " + e.getMessage());
     } finally {
       try {
-        if (clientSocket != null) {
-          clientSocket.close();
-        }
+        clientSocket.close();
       } catch (IOException e) {
         System.out.println("IOException: " + e.getMessage());
       }
